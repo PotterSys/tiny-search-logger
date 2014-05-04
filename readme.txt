5ed8c7caba0f22be01d7dc09525a75a8b74193b3
PotterSys <pottersys@gmail.com>
2014-05-04 02:12:34 -0400

    Added license and other community information

---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== Tiny Search Logger ===
+Contributors: PotterSys
+Tags: search, logging
+Requires at least: 3.8
+Tested up to: 3.9
+Stable tag: 1
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Logs searches made by end users on the Search form. Use this wisely
+
+== Installation ==
+
+1. Upload the plugin folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Now all the searches made on your site will appear on Searches menu in your WordPress administration panel
+
+== Changelog ==
+
+= 1 =
+* Adding license and other community info
+
+= 0.1 =
+* Development version
--- search-log-main.php
+++ search-log-main.php
@@ -4,10 +4,33 @@ Plugin Name: Tiny Search Logger
 Plugin URI: http://www.pslabs.cl/
 Description: Logs searches made by end users. Use wisely
 Author: Juan Correa
-Version: 0.1
+Version: 1
 Author URI: http://www.pslabs.cl/
 Text Domain: pslabs-tiny-search-logger
+License: GNU GPL v2
 */
+
+/**
+ *  Tiny Search Logger
+    Copyright (C) 2014 - Juan Correa Poblete
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    
+    Contact in juan *at/@* pslabs.cl
+ *
+ **/
 
 register_activation_hook( __FILE__, 'ps_log_install_plugin' );
 
